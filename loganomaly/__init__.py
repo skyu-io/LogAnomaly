d640a28986d796af4b241f7c3ba0a16ee58d60da
Dinusha Ambagahawita <dinusha.ambagahawita@insighture.com>
2025-10-14 19:19:06 +0530

    minor updates to remove the unused library and adding default check when no anomalies

--- loganomaly/__init__.py
+++ loganomaly/__init__.py
@@ -11,7 +11,7 @@ __version__ = "0.1.0"
 # Import main components
 from loganomaly.detectors import compute_lof_scores, mark_lof_anomalies
 from loganomaly.llm_classifier import classify_log_llm, LLM_STATS
-from loganomaly.workflow import WorkflowContext, LogAnalysisWorkflow, classify_log_with_pipeline
+from loganomaly.workflow import WorkflowContext, LogAnalysisWorkflow
 
 __all__ = [
     'compute_lof_scores',
@@ -19,6 +19,5 @@ __all__ = [
     'classify_log_llm',
     'LLM_STATS',
     'LogAnalysisWorkflow',
-    'WorkflowContext',
-    'classify_log_with_pipeline'
+    'WorkflowContext'
 ]
